cc5962f69c30faff5ebde35fc2d3bf64d59c7cf6
Walter Cruz <walter.php@gmail.com>
2013-05-22 11:22:37 -0300

    atualização de código - atualizando mg e pr

--- import.py
+++ import.py
@@ -3,6 +3,34 @@ import glob
 import sys
 f = open('municipios.sql','w+')
 
+estados = {'AC':'1',
+		   'AL':'2',
+		   'AM':'3',
+		   'AP':'4',
+		   'BA':'5',
+		   'CE':'6',
+		   'DF':'7',
+		   'ES':'8',
+		   'GO':'9',
+		   'MA':'10',
+		   'MG':'11',
+		   'MS':'12',
+		   'MT':'13',
+		   'PA':'14',
+		   'PB':'15',
+		   'PE':'16',
+		   'PI':'17',
+		   'PR':'18',
+		   'RJ':'19',
+		   'RN':'20',
+		   'RO':'21',
+		   'RR':'22',
+		   'RS':'23',
+		   'SC':'24',
+		   'SE':'25',
+		   'SP':'26',
+		   'TO':'27'}
+
 arquivos = glob.glob('estados/*')
 sql = []
 
@@ -16,7 +44,10 @@ for arquivo in arquivos:
 	for i in doc.xpath("//*[@class='nome']"):
 		municipioNome = i.text
 		municipioId = i.getnext().text
-		sql.append('INSERT INTO municipios (codigo,nome,estado) VALUES ('+ municipioId +',"'+municipioNome+'","'+estadoSigla+'");')
-
+		try:
+			sql.append('INSERT INTO municipios (codigo,nome,fk_estado) VALUES ('+ str(int(municipioId)) +',"'+municipioNome+'","'+estados[estadoSigla]+'");')
+		except:
+			pass
+			#print(municipioId+ '-'+ estadoSigla)
 #print sql
 f.write('\n'.join(sql).encode('utf8'))
